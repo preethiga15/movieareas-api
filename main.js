ec5f8a8933046b8fd7a7f27d1bf3401b332ad511
PreethigaKV <preethiga@bharath.ac.in>
2024-06-25 15:44:26 +0530

    route structure

--- main.js
+++ main.js
@@ -7,6 +7,17 @@ app.get("/", (req, res) => {
   res.json({ message: "Hello World" });
 });
 
+//CRUD functionality of movies
+
+//R-For Reading
+app.get('/movies', () => {});
+
+//c-For creating movies
+app.post("/movies",() => {});
+
+
+
+
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
