b93aea429a22ec94d32bebfcb2965093b5afef80
PreethigaKV <preethiga@bharath.ac.in>
2024-06-26 14:56:32 +0530

    add controller files

--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -1,20 +1,22 @@
 import express from "express";
-
+import { movieIndex,
+    movieCreate,
+    movieUpdate,
+    moviedelete,
+} from "../controller/movies.controller";
 
 const router = express.Router();
 
 //CRUD functionality of movies
 
 //R-For Reading
-router.get('/', (req,res) => {
-    res.send("get all movies listtt");
-});
+router.get("/", movieIndex);
 
 //c-For creating movies
-router.post("/",() => {});
+router.post("/",movieCreate);
 
-router.put("/:id", () => {});
+router.put("/:id",movieUpdate);
 
-router.delete("/:id", () => {});
+router.delete("/:id",moviedelete);
 
  export default router;
